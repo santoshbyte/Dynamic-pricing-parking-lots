e84809dca564600bcbd02ee0df0ca578bc16e251
santoshbyte <santoshmallick447@gmail.com>
2025-07-09 22:47:49 +0530

    Create ProjectStructure.cpp

---
+++ ProjectStructure.cpp
@@ -0,0 +1,8 @@
+project-root/
+ ┣ 📄 main_pipeline.py
+ ┣ 📄 preprocess_data.ipynb
+ ┣ 📄 cleaned.csv
+ ┣ 📄 streamed_output.jsonl
+ ┣ 📄 architecture.png (or .md if mermaid)
+ ┣ 📄 README.md
+ ┗ 📄 report.pdf (optional)
