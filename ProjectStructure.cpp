2582005fda6c35b8bf72eda4fa2b9a75e276acf1
santoshbyte <santoshmallick447@gmail.com>
2025-07-09 22:50:58 +0530

    Update and rename ProjectStructure.cpp to ProjectStructure

--- ProjectStructure.cpp
+++
@@ -1,8 +0,0 @@
-project-root/
- ┣ 📄 main_pipeline.py
- ┣ 📄 preprocess_data.ipynb
- ┣ 📄 cleaned.csv
- ┣ 📄 streamed_output.jsonl
- ┣ 📄 architecture.png (or .md if mermaid)
- ┣ 📄 README.md
- ┗ 📄 report.pdf (optional)
